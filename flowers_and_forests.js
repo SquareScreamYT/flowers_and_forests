e296726aed40bef8d996c170bcc6393a3b3377e7
SquareScreamYT <134925668+SquareScreamYT@users.noreply.github.com>
2024-01-28 16:55:10 +0800

    Update flowers_and_forests.js

--- flowers_and_forests.js
+++ flowers_and_forests.js
@@ -1,3 +1,18 @@
 /* Flowers And Forests
 Created by Pixelegend4 and SquareScreamYT
 */
+
+elements.nutmeg = {
+	color: "#b86d42",
+	behavior: behaviors.POWDER,
+	category: "land",
+	state: "solid",
+            breakInto: “nutmeg_ground”,
+};
+
+elements.nutmeg_ground = {
+	color: "#804d2f",
+	behavior: behaviors.POWDER,
+	category: "land",
+	state: "solid",
+};
